c6ea9b5b5264a3eec8d12281e413189094d272a6
subugee <sasubramanian_md@hotmail.com>
2024-04-05 18:03:32 +0530

    styling,data fetching,api

---
+++ nextapps/rest-api/src/app/api/greeter/[message]/route.js
@@ -0,0 +1,7 @@
+
+import { NextResponse } from "next/server";
+
+export async function GET(request, { params: {message} }) {
+    // console.log(obj.params.message)
+    return NextResponse.json({ message })
+}
---
+++ nextapps/styling-app/src/app/dashboard/layout.js
@@ -0,0 +1,8 @@
+// import styles from './dashboard.module.css'
+import styles from '@/app/dashboard/dashboard.module.css'
+
+export default function DashboardLayout({children}) {
+    return <div className={styles.dashboard}>
+            {children}
+    </div>
+}
